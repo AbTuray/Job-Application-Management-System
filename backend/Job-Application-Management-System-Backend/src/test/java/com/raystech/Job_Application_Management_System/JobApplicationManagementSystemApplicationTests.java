79a62d992059261f7c81084ce9f54a87199669c1
AbTuray <turayabdulai570@gmail.com>
2025-11-14 13:55:15 +0000

    Creating Project Folder

---
+++ backend/Job-Application-Management-System-Backend/src/test/java/com/raystech/Job_Application_Management_System/JobApplicationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raystech.Job_Application_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
